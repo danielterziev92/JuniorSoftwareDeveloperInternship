1db45da4e9fec6550e71fcf5299e2b5b216e1e6d
danielterziev92 <daniel.st.terziev@gmail.com>
2024-01-21 21:31:43 +0200

    Add text.txt file where collect text witch pass to my function. Also write tests for the function and test all patterns.

--- InternetSecuritiesBulgaria/task1/solution.py
+++ InternetSecuritiesBulgaria/task1/solution.py
@@ -1,19 +1,10 @@
 import re
 
 
-def get_all_date(text):
-    date_with_dashes = r'\b\d{4}-\d{2}-\d{2}\b'
-    date_with_backslashes = r'\b\d{4}/\d{2}/\d{2}\b'
-    date_with_month_name_and_year = r'\b(?:January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{1,2}(?:st|nd|rd|th)?,\s+\d{4}\b'
-    date_with_months = r'\b(?:January|February|March|April|May|June|July|August|September|October|November|December)\b'
-    date_with_month_and_hour = r'\b(?:January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{1,2}(?:h)?\b'
-
-    all_pattern = [date_with_dashes, date_with_backslashes, date_with_months, date_with_month_name_and_year,
-                   date_with_month_and_hour]
-
+def get_all_date(patterns, text):
     matches = []
 
-    for pattern in all_pattern:
+    for pattern in patterns.values():
         match = re.findall(pattern, text)
         if match:
             matches.extend(match)
@@ -21,20 +12,25 @@ def get_all_date(text):
     return set(matches)
 
 
-def main():
-    # Sample text
-    text = """
-    Hi, 
-    my name is Jane and my phone number is 555-123-4567. 
-    My email address is jane_doe@example.com. 
-    I live on 123 Main St. Apt. #456, and I was born on January 11th, 1990. 
-    I have an appointment on 2023-05-15 at 2:30pm at 789 Oak Ln. #3 and backup on 2023/05/21. 
-    Please give me a call or send me an email to confirm. In case the dates are unavailable, please set up a meeting sometime in June. I would love June 19h.
-    Thank you!
-    """
+def get_text_from_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        file_content = file.read().replace('\n', '')
+        return file_content
 
-    dates = get_all_date(text)
-    print(dates)
+
+def main():
+    file_path = 'text.txt'
+    text = get_text_from_file(file_path)
+
+    all_patterns = {
+        'date_with_dashes': r'\b\d{4}-\d{2}-\d{2}\b',
+        'date_with_backslashes': r'\b\d{4}/\d{2}/\d{2}\b',
+        'date_with_months': r'\b(?:January|February|March|April|May|June|July|August|September|October|November|December)\b',
+        'date_with_month_and_hour': r'\b(?:January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{1,2}(?:h)?\b',
+        'date_with_month_name_and_year': r'\b(?:January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{1,2}(?:st|nd|rd|th)?,\s+\d{4}\b'
+    }
+
+    print(get_all_date(all_patterns, text))
 
 
 if __name__ == "__main__":
---
+++ InternetSecuritiesBulgaria/task1/tests.py
@@ -0,0 +1,66 @@
+from unittest import TestCase
+
+from task1.solution import get_all_date
+
+
+class TestTask1(TestCase):
+
+    def setUp(self):
+        test_text = '''
+Hi, 
+my name is Jane and my phone number is 555-123-4567. 
+My email address is jane_doe@example.com. 
+I live on 123 Main St. Apt. #456, and I was born on January 11th, 1990. 
+I have an appointment on 2023-05-15 at 2:30pm at 789 Oak Ln. #3 and backup on 2023/05/21. 
+Please give me a call or send me an email to confirm. In case the dates are unavailable, please set up a meeting sometime in June. I would love June 19h.
+Thank you!
+        '''
+
+        self.text = test_text.replace('\n', '')
+
+    def test_date_with_dashes_pattern(self):
+        patterns = {'date_with_dashes': r'\b\d{4}-\d{2}-\d{2}\b', }
+
+        result = get_all_date(patterns, self.text)
+        expected_result = '2023-05-15'
+
+        self.assertIn(expected_result, result)
+
+    def test_date_with_backslashes(self):
+        patterns = {'date_with_backslashes': r'\b\d{4}/\d{2}/\d{2}\b', }
+
+        result = get_all_date(patterns, self.text)
+        expected_result = '2023/05/21'
+
+        self.assertIn(expected_result, result)
+
+    def test_date_with_month_name(self):
+        patterns = {
+            'date_with_months': r'\b(?:January|February|March|April|May|June|July|August|September|October|November|December)\b',
+        }
+
+        result = get_all_date(patterns, self.text)
+        expected_results = ['June', 'January']
+
+        for expected_result in expected_results:
+            self.assertIn(expected_result, result)
+
+    def test_date_with_month_and_hour(self):
+        patterns = {
+            'date_with_month_and_hour': r'\b(?:January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{1,2}(?:h)?\b',
+        }
+
+        result = get_all_date(patterns, self.text)
+        expected_result = 'June 19h'
+
+        self.assertIn(expected_result, result)
+
+    def test_date_with_month_day_and_year(self):
+        patterns = {
+            'date_with_month_name_and_year': r'\b(?:January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{1,2}(?:st|nd|rd|th)?,\s+\d{4}\b'
+        }
+
+        result = get_all_date(patterns, self.text)
+        expected_result = 'January 11th, 1990'
+
+        self.assertIn(expected_result, result)

7f3d84ea57cbe3ec86f52363fafad452f94b9a8b
danielterziev92 <daniel.st.terziev@gmail.com>
2024-01-21 21:41:34 +0200

    Add test to test get text from file function

--- InternetSecuritiesBulgaria/task1/tests.py
+++ InternetSecuritiesBulgaria/task1/tests.py
@@ -1,20 +1,20 @@
 from unittest import TestCase
 
-from task1.solution import get_all_date
+from task1.solution import get_all_date, get_text_from_file
 
 
 class TestTask1(TestCase):
 
     def setUp(self):
         test_text = '''
-Hi, 
-my name is Jane and my phone number is 555-123-4567. 
-My email address is jane_doe@example.com. 
-I live on 123 Main St. Apt. #456, and I was born on January 11th, 1990. 
-I have an appointment on 2023-05-15 at 2:30pm at 789 Oak Ln. #3 and backup on 2023/05/21. 
-Please give me a call or send me an email to confirm. In case the dates are unavailable, please set up a meeting sometime in June. I would love June 19h.
-Thank you!
-        '''
+Hi,
+my name is Jane and my phone number is 555-123-4567.
+My email address is jane_doe@example.com.
+I live on 123 Main St. Apt. #456, and I was born on January 11th, 1990.
+I have an appointment on 2023-05-15 at 2:30pm at 789 Oak Ln. #3 and backup on 2023/05/21.
+Please give me a call or send me an email to confirm. In case the dates are unavailable, please set up a meeting 
+sometime in June. I would love June 19h.Thank you!
+'''
 
         self.text = test_text.replace('\n', '')
 
@@ -64,3 +64,13 @@ Thank you!
         expected_result = 'January 11th, 1990'
 
         self.assertIn(expected_result, result)
+
+    def test_get_text_from_file(self):
+        file_path = 'text.txt'
+
+        result = get_text_from_file(file_path)
+        expected_result = self.text
+
+        self.maxDiff = None
+
+        self.assertEqual(expected_result, result)
